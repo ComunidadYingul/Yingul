9d72559500cf85a67dbe664e37dfcca1a8faa332
eddy <quenallataeddy@gmail.com>
2018-02-06 15:56:32 -0400

    filtro de la parte izquierda

--- src/main/java/com/valework/yingul/controller/CategoryController.java
+++ src/main/java/com/valework/yingul/controller/CategoryController.java
@@ -1,14 +1,10 @@
 package com.valework.yingul.controller;
 
-import java.util.Collections;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.valework.yingul.dao.CategoryDao;
 import com.valework.yingul.model.Yng_Category;
 import com.valework.yingul.service.CategoryService;
 
