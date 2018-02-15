165dcad2cb7d9cd189c32fe41b664f00867a987e
eddy <quenallataeddy@gmail.com>
2018-02-15 08:40:33 -0400

    antes de merge

--- src/main/java/com/valework/yingul/controller/UserController.java
+++ src/main/java/com/valework/yingul/controller/UserController.java
@@ -1,20 +1,33 @@
 package com.valework.yingul.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.valework.yingul.dao.UserDao;
+import com.valework.yingul.model.Yng_Item;
+import com.valework.yingul.model.Yng_Person;
 import com.valework.yingul.model.Yng_User;
+import com.valework.yingul.service.PersonService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	@Autowired 
 	UserDao userDao;
-	
+	@Autowired 
+	PersonService personService;
 	@RequestMapping("/{username}")
     public Yng_User findByUsername(@PathVariable("username") String username) {
         return userDao.findByUsername(username);
+    }
+	@RequestMapping("/person/{username}")
+    public Yng_Person getPerson(@PathVariable("username") String username) {
+		Yng_User yng_User = userDao.findByUsername(username); 
+		List<Yng_Person> personList= personService.findByUser(yng_User);
+		Yng_Person person = personList.get(0);
+		return person;	
     }
 }
---
+++ src/main/java/com/valework/yingul/dao/FavoriteDao.java
@@ -0,0 +1,15 @@
+package com.valework.yingul.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import com.valework.yingul.model.Yng_Favorite;
+import com.valework.yingul.model.Yng_Item;
+import com.valework.yingul.model.Yng_User;
+
+public interface FavoriteDao extends CrudRepository<Yng_Favorite, Long> {
+	Yng_Favorite findByFavoriteId(Long favoriteId);
+	Yng_Favorite findByItemAndUser(Yng_Item item, Yng_User user);
+	List<Yng_Favorite> findByUserOrderByFavoriteIdDesc(Yng_User user);
+	
+}
