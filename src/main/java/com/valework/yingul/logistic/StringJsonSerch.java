f60383bb8049009b9b456a24de579814c84b021f
danielChoque <patzidaniel@gmail.com>
2018-04-11 17:13:29 -0400

    corregido build

--- src/main/java/com/valework/yingul/logistic/StringJsonSerch.java
+++ src/main/java/com/valework/yingul/logistic/StringJsonSerch.java
@@ -1,6 +1,6 @@
 package com.valework.yingul.logistic;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
+
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,7 +14,7 @@ import java.io.File;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
