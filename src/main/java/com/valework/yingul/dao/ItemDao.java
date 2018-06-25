c95d39a55f9cf62b705d639a3cab30c964224a23
Eddy <quenallataeddy@gmail.com>
2018-06-25 10:13:45 -0400

    request con limites

--- src/main/java/com/valework/yingul/dao/ItemDao.java
+++ src/main/java/com/valework/yingul/dao/ItemDao.java
@@ -9,4 +9,8 @@ public interface ItemDao extends CrudRepository<Yng_Item, Long> {
 	//List<Yng_Item> findAll();
 	List<Yng_Item> findByOrderByItemIdDesc();
 	List<Yng_Item> findByIsOverOrderByItemIdDesc(boolean sw);
+	List<Yng_Item> findByIsOverOrderByItemIdAsc(boolean sw);
+	List<Yng_Item> findByIsOverAndTypeOrderByItemIdDesc(boolean over, String type);
+	List<Yng_Item> findByIsOverAndTypeOrderByItemIdAsc(boolean over, String type);
+	
 }
