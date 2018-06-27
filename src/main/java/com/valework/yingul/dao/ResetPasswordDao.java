6968085c4ac570b2d0ac75d06ee6a585639db009
Eddy <quenallataeddy@gmail.com>
2018-06-27 12:01:17 -0400

    recuperacion de password en android

--- src/main/java/com/valework/yingul/dao/ResetPasswordDao.java
+++ src/main/java/com/valework/yingul/dao/ResetPasswordDao.java
@@ -6,5 +6,6 @@ import com.valework.yingul.model.Yng_User;
 
 public interface ResetPasswordDao extends CrudRepository<Yng_ResetPassword, Long>{
 	Yng_ResetPassword findByUser(Yng_User user);
+	Yng_ResetPassword findByUserAndCodeResetPassword(Yng_User user,int codeResetPassword);
 	Yng_ResetPassword findByResetpasswordId(Long resetPasswordId);
 }
